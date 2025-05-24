686fc2143889a6834e6033c29982b681c1cd764a
GarryChen <chenymj23@gmail.com>
2025-05-24 19:14:01 +0200

    change dir add C++

---
+++ C++/common/shape.h
@@ -0,0 +1,60 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include <stdio.h>
+#include <stdexcept>
+
+enum class shape_type {
+    circle,
+    triangle,
+    rectangle,
+};
+
+class shape {
+public:
+    virtual ~shape() {}
+};
+
+class circle : public shape {
+public:
+    circle() {
+        puts("circle()");
+    }
+    ~circle() {
+        puts("~circle()");
+    }
+};
+
+class triangle : public shape {
+public:
+    triangle() {
+        puts("triangle()");
+    }
+    ~triangle() {
+        puts("~triangle()");
+    }
+};
+
+class rectangle : public shape {
+public:
+    rectangle() {
+        puts("rectangle()");
+    }
+    ~rectangle() {
+        puts("~rectangle()");
+    }
+};
+
+inline shape* create_shape(shape_type type) {
+    switch (type) {
+        case shape_type::circle:
+            return new circle();
+        case shape_type::triangle:
+            return new triangle();
+        case shape_type::rectangle:
+            return new rectangle();
+    }
+    throw std::logic_error("Unknown shape type");
+}
+
+#endif // SHAPE_H

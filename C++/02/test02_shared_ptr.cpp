eb80584e6d045b8f5db83ba811c5cd48292e6888
GarryChen <chenymj23@gmail.com>
2025-06-24 19:14:23 +0800

    Refactor smart pointer implementation and update usage count print statements for clarity

--- C++/02/test02_shared_ptr.cpp
+++ C++/02/test02_shared_ptr.cpp
@@ -6,17 +6,17 @@
 int main()
 {
     smart_ptr<circle> ptr1(new circle());
-    printf("ptr1 use count: %ld\n", ptr1.use_count());
+    printf("use count of pt1 is %ld\n", ptr1.use_count());
 
     smart_ptr<circle> ptr2;
-    printf("ptr2 use count: %ld\n", ptr2.use_count());
+    printf("use count of ptr2 was %ld\n", ptr2.use_count());
     ptr2 = ptr1; // copy assignment
-    printf("ptr2 now is %ld\n", ptr2.use_count());
+    printf("use count of ptr2 now is %ld\n", ptr2.use_count());
     if (ptr1)
     {
-        puts("ptr1 is not null");
+        puts("ptr1 is not empty");
     }
 
     smart_ptr<circle> ptr3 = dynamic_pointer_cast<circle>(ptr2);
-    printf("ptr3 use count: %ld\n", ptr3.use_count());
+    printf("use count of ptr3 is %ld\n", ptr3.use_count());
 }
